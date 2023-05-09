cb4badf7ab96ef9edda56bcc99c4d4a0d27d9d35
Surasit20 <surasit.yo20@gmail.com>
2023-05-09 11:30:02 +0700

    first commit

---
+++ Backend/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Supplier { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+   
+   
+    }
+}
---
+++ Shared/Supplier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string TaxID { get; set; }
+        public string OfficeName { get; set; }
+
+        
+
+    }
+}
